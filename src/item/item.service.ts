4a09c9fa79c23956674f2732a0bd4c4e3d3a1424
Sudeep Chowdhury <sudeep@itobuz.com>
2024-12-24 14:56:12 +0530

    feature: HM-145 code formated.

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -13,7 +13,7 @@ import { UniqueIdentifierInput } from "src/types/inputtypes/unique-id.input";
 
 @Injectable()
 export class ItemService {
-  constructor(private readonly prisma: PrismaService) { }
+  constructor(private readonly prisma: PrismaService) {}
 
   async createItem(ctx, createItemInput: CreateItemInput) {
     const {
@@ -190,9 +190,9 @@ export class ItemService {
       const uploadData = item.uploadRelation?.[0]?.upload;
       const image = uploadData
         ? {
-          ...uploadData,
-          fileUrl: `${backendBaseUrl}/uploads/${uploadData.file}`,
-        }
+            ...uploadData,
+            fileUrl: `${backendBaseUrl}/uploads/${uploadData.file}`,
+          }
         : null;
       return { ...item, image };
     } catch (error) {
@@ -268,8 +268,9 @@ export class ItemService {
       });
 
       return {
-        message: `Item ${!itemPresence.status === true ? "Enabled" : "Disabled"
-          }!`,
+        message: `Item ${
+          !itemPresence.status === true ? "Enabled" : "Disabled"
+        }!`,
       };
     } catch (error) {
       console.log("Error=", error);
